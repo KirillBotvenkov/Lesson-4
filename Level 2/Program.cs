8ec003cec7369e7492ca314fe393b3d14c2e8d40
Kirill <botvenkov2016@yandex.ru>
2023-01-18 20:12:13 +0300

    Задача 2

---
+++ Level 2/Program.cs
@@ -0,0 +1,33 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+
+int number = ReadLine("Введите число: ");
+int b = Numberb(number);
+SumNumbers(number, b);
+
+int ReadLine(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Numberb(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+void SumNumbers(int n, int b)
+{
+    int sum = 0;
+    for (int i = 1; i <= b; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
+}
