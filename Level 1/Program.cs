1ec817609065deeb721a1114fc737dde2bd2ce2b
Kirill <botvenkov2016@yandex.ru>
2023-01-18 20:10:03 +0300

    Задача 1

---
+++ Level 1/Program.cs
@@ -0,0 +1,35 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+
+int GetNumber(string message)
+{
+int result = 0;
+
+while(true)
+{
+Console.WriteLine(message);
+
+if(int.TryParse(Console.ReadLine(), out result) && result >= 1)
+{
+break;
+}
+else
+{
+Console.WriteLine("ввели не число или меньше 1.");
+}
+}
+
+return result;
+}
+
+int number = GetNumber("Введите число больше 1");
+int number2 = GetNumber("Введите число больше 1");
+int result= 1;
+{
+    for (int i = 1; i <= number2; i++)
+    {
+        result = result * number;
+    }
+
+Console.WriteLine($"Число {number} в степени {number2} = {result}");
+}
+return result;
